83ebee3fa4d5723a050400e7755136345292b05c
sagar saud <sagarsaud67@gmail.com>
2025-01-14 22:51:59 +0545

    all files are added to git

---
+++ components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <header>
+      <img src="../../src/assets/logo.png" alt="logo" />
+      <span>Chef.AI</span>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hero.css";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h2>
+        I’m Your Recipe <br /> Specialist
+      </h2>
+      <p>Tell me your ingredients, and I’ll find the perfect recipe for you.</p>
+    </div>
+  );
+};
+
+export default Hero;
